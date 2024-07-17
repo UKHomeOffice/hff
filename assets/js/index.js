0b8b3b2e556be003b21bf11f16d150cd0c826e77
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-07-17 09:33:13 +0100

    Remove jquery and a11y autocomplete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,3 @@
 'use strict';
 
 require('hof/frontend/themes/gov-uk/client-js');
-const $ = require('jquery');
-const accessibleAutocomplete = require('accessible-autocomplete');
-
-$('.typeahead').each(function applyTypeahead() {
-  accessibleAutocomplete.enhanceSelectElement({
-    defaultValue: '',
-    selectElement: this
-  });
-});
