1c78b90f67248491d2299ce4cbebe754f3823ee7
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-07-22 14:07:06 +0100

    TLF-240 Update steps and fields

--- apps/hff/fields/index.js
+++ apps/hff/fields/index.js
@@ -1,7 +1,23 @@
 'use strict';
 
 module.exports = {
-  feedback: {
-    validate: 'required'
+  satisfaction: {
+    mixin: 'radio-group',
+    options: [
+      'very-satisfied',
+      'satisfied',
+      'neither-satisfied-or-dissatisfied',
+      'dissatisfied',
+      'very-dissatisfied'
+    ]
+  },
+  improvements: {
+    mixin: 'textarea',
+    attributes: [{ attribute: 'rows', value: 5 }],
+    validate: [
+      'notUrl',
+      { type: 'regex', arguments: /^[^\[\]\|<>]*$/ },
+      { type: 'maxlength', arguments: 10000 }
+    ]
   }
 };
--- apps/hff/index.js
+++ apps/hff/index.js
@@ -1,12 +1,19 @@
 'use strict';
+const getServiceQuery = require('./behaviours/get-service-query');
+const submitFeedback = require('./behaviours/submit-feedback');
 
 module.exports = {
   name: 'hff',
   baseUrl: '/',
   steps: {
     '/feedback': {
-      fields: [],
+      fields: ['satisfaction', 'improvements'],
+      behaviours: [getServiceQuery, submitFeedback],
       next: '/feedback-sent'
+    },
+    '/feedback-sent': {
+      clearSession: true,
+      backLink: false
     }
   }
 };
