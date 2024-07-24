05f656dd9caaf28f39c84a61760eff302bf7974e
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-07-24 13:56:14 +0100

    TLF-240 further improve validations. Update tests

--- apps/hff/behaviours/get-service-query.js
+++ apps/hff/behaviours/get-service-query.js
@@ -3,13 +3,26 @@ const serviceReferrerNameRegex = require('../../../config').regex.serviceReferre
 module.exports = superclass => class extends superclass {
   configure(req, res, next) {
     const { form, returnUrl } = req.query;
+    const referrer = req.get('referrer');
 
-    if (serviceReferrerNameRegex.test(form)) {
+    if (typeof form === 'string' && serviceReferrerNameRegex.test(form)) {
       req.sessionModel.set('service-referrer-name', form);
     }
 
-    if (URL.canParse(returnUrl) && returnUrl.endsWith('.homeoffice.gov.uk')) {
-      req.sessionModel.set('service-referrer-url', returnUrl);
+    if (URL.canParse(referrer)) {
+      const referrerUrl = new URL(referrer);
+      const { origin } = referrerUrl;
+      if (typeof origin === 'string' && origin.endsWith('.homeoffice.gov.uk')) {
+        req.sessionModel.set('service-referrer-url', origin);
+      }
+    }
+
+    if (URL.canParse(returnUrl)) {
+      const serviceUrl = new URL(returnUrl);
+      const { origin } = serviceUrl;
+      if (typeof origin === 'string' && origin.endsWith('.homeoffice.gov.uk')) {
+        req.sessionModel.set('service-referrer-url', origin);
+      }
     }
 
     super.configure(req, res, next);
--- test/unit/behaviours/get-service-query.test.js
+++ test/unit/behaviours/get-service-query.test.js
@@ -32,10 +32,8 @@ describe('get-service-query behaviour', () => {
 
       req.query = {
         form: 'ASC',
-        returnUrl: 'https://...'
+        returnUrl: 'https://www.fake-service.homeoffice.gov.uk'
       };
-
-      instance.configure(req, res, next);
     });
 
     afterEach(() => {
@@ -43,12 +41,60 @@ describe('get-service-query behaviour', () => {
     });
 
     test('should be called', () => {
+      instance.configure(req, res, next);
       expect(Base.prototype.configure).toHaveBeenCalled();
     });
 
     test('should add req.query values to the session under correct keys', () => {
+      instance.configure(req, res, next);
       expect(req.sessionModel.get('service-referrer-name')).toBe('ASC');
-      expect(req.sessionModel.get('service-referrer-url')).toBe('https://...');
+      expect(req.sessionModel.get('service-referrer-url')).toBe('https://www.fake-service.homeoffice.gov.uk');
+    });
+
+    test('should not add a service name query value to session if it does not match the proper format', () => {
+      req.query = {
+        form: 'ASC!',
+        returnUrl: 'https://www.fake-service.homeoffice.gov.uk'
+      };
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-name')).toBe(undefined);
+    });
+
+    test('should not add a returnUrl query value to session if it does not match the proper format', () => {
+      req.query = {
+        form: 'ASC',
+        returnUrl: 'https://www.fake-service.attacker.com'
+      };
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe(undefined);
+      req.query = {
+        form: 'ASC',
+        returnUrl: 'fake-service.homeoffice.gov.uk'
+      };
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe(undefined);
+    });
+
+    test('Referrer value will be added to session if it matches the proper format', () => {
+      req.query = {
+        form: 'ASC'
+      };
+      req.get = jest.fn().mockReturnValue('https://fake-service.homeoffice.gov.uk');
+      // req.referrer = 'https://www.fake-service.homeoffice.gov.uk'
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe('https://fake-service.homeoffice.gov.uk');
+    });
+
+    test('Referrer value won\'t be added to session if it does not match the proper format', () => {
+      req.query = {
+        form: 'ASC'
+      };
+      req.get = jest.fn().mockReturnValue('fake-service.homeoffice.gov.uk');
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe(undefined);
+      req.get = jest.fn().mockReturnValue('https://www.fake-service.attacker.com');
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe(undefined);
     });
   });
 });
