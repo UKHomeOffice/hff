f0f313b748797320fa8bb5e3aca1937cb4b3ccae
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-07-24 16:39:47 +0100

    TLF-240 Add test
    * Return value overrides referrer value if both are present in form context.

--- test/unit/behaviours/get-service-query.test.js
+++ test/unit/behaviours/get-service-query.test.js
@@ -96,5 +96,12 @@ describe('get-service-query behaviour', () => {
       instance.configure(req, res, next);
       expect(req.sessionModel.get('service-referrer-url')).toBe(undefined);
     });
+
+    test('returnUrl value overrides Referrer value in session if both are present', () => {
+      req.query.returnUrl = 'https://return-service.homeoffice.gov.uk'
+      req.get = jest.fn().mockReturnValue('https://referrer-service.homeoffice.gov.uk');
+      instance.configure(req, res, next);
+      expect(req.sessionModel.get('service-referrer-url')).toBe('https://return-service.homeoffice.gov.uk');
+    });
   });
 });
