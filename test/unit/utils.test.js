19cb9ef140e8891885791d312b01aaf394af5fcf
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-07-29 12:29:49 +0100

    TLF-240 digest generator throws if vital arguments are missing
    Updated tests with new scenarios

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,4 +1,4 @@
-const { prettyPrintSentence } = require('../../utils');
+const { prettyPrintSentence, createHmacDigest } = require('../../utils');
 
 describe('Utilities \'prettyPrintSentence\' ', () => {
   test('replaces all hyphens in a string with spaces', () => {
@@ -21,5 +21,45 @@ describe('Utilities \'prettyPrintSentence\' ', () => {
     expect(() => prettyPrintSentence(undefined)).toThrow(new Error('Cannot pretty print a non string parameter'));
     expect(() => prettyPrintSentence(12345)).toThrow(new Error('Cannot pretty print a non string parameter'));
     expect(() => prettyPrintSentence()).toThrow(new Error('Cannot pretty print a non string parameter'));
+  });
+});
+
+describe('Utilities \'createHmacDigest\' ', () => {
+  test('returns a string being of the correct length', () => {
+    const newMac = createHmacDigest('sha256', 'skeletonKey', 'message to hash', 'hex');
+    expect(typeof newMac).toBe('string');
+    expect(newMac.length).toBe(64);
+  });
+
+  test('HMACs of different messages cannot be matched', () => {
+    const mac1 = createHmacDigest('sha256', 'skeletonKey', 'message one', 'hex');
+    const mac2 = createHmacDigest('sha256', 'skeletonKey', 'message two', 'hex');
+    expect(mac1 === mac2).toBe(false);
+  });
+
+  test('HMACs signed with different keys cannot be matched', () => {
+    const mac1 = createHmacDigest('sha256', 'skeletonKey', 'my message', 'hex');
+    const mac2 = createHmacDigest('sha256', 'fakeSkeletonKey', 'my message', 'hex');
+    expect(mac1 === mac2).toBe(false);
+  });
+
+  test('returns an error when algorithm parameter is not supplied', () => {
+    expect(() => createHmacDigest(null, 'skeletonKey', 'message text', 'hex'))
+      .toThrow(new Error('You must provide a hashing algorithm e.g. \'sha256\''));
+  });
+
+  test('returns an error when key parameter is not supplied', () => {
+    expect(() => createHmacDigest('sha256', null, 'message text', 'hex'))
+      .toThrow(new Error('You must provide a HMAC key'));
+  });
+
+  test('returns an error when message parameter is not supplied', () => {
+    expect(() => createHmacDigest('sha256', 'skeletonKey', null, 'hex'))
+      .toThrow(new Error('You must provide a message to hash'));
+  });
+
+  test('returns an error when encoding parameter is not supplied', () => {
+    expect(() => createHmacDigest('sha256', 'skeletonKey', 'message text'))
+      .toThrow(new Error('You must provide an output encoding e.g. \'hex\''));
   });
 });
--- utils/index.js
+++ utils/index.js
@@ -9,6 +9,15 @@ const prettyPrintSentence = string => {
 
 // Create a HMAC digest from key and message
 const createHmacDigest = (algorithm, key, message, encoding) => {
+  if (!algorithm) {
+    throw new Error('You must provide a hashing algorithm e.g. \'sha256\'');
+  } else if (!key) {
+    throw new Error('You must provide a HMAC key');
+  } else if (!message) {
+    throw new Error('You must provide a message to hash');
+  } else if (!encoding) {
+    throw new Error('You must provide an output encoding e.g. \'hex\'');
+  }
   return createHmac(algorithm, key).update(message).digest(encoding);
 };
 
