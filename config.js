a5c0db46790db5e98f7d9885d1848dfa30c675c0
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-08-02 16:32:16 +0100

    TLF-240 remove unused config values

--- config.js
+++ config.js
@@ -4,16 +4,11 @@
 const env = process.env.NODE_ENV || 'production';
 
 module.exports = {
-  PRETTY_DATE_FORMAT: 'DD MMMM YYYY',
-  dateTimeFormat: 'DD MMM YYYY HH:mm:ss',
   env: env,
   govukNotify: {
     notifyApiKey: process.env.NOTIFY_KEY,
     feedbackInbox: process.env.FEEDBACK_INBOX,
     feedbackSubmissionTemplateId: process.env.FEEDBACK_TEMPLATE_ID
-  },
-  hosts: {
-    acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
   },
   redis: {
     port: process.env.REDIS_PORT || '6379',
